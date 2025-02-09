ce725c158048787933da0c82ee70e3fe295ce002
Jochim Van Dijck <jochim.van.dijck@gmail.com>
2025-02-09 18:10:23 +0100

    update keymaps

--- .config/nvim/plugin/keymaps.lua
+++ .config/nvim/plugin/keymaps.lua
@@ -1,13 +1,17 @@
 local set = vim.keymap.set
 
-set("n", "<space><space>x", "<cmd>source %<CR>")
-set("n", "<space>x", ":.lua<CR>")
-set("v", "<space>x", ":lua<CR>")
+set("n", "<leader><leader>x", "<cmd>source %<CR>", { desc = "Source current file" })
+set("n", "<leader>x", "<cmd>.lua<CR>", { desc = "Source current line" })
+set("v", "<leader>x", "<cmd>lua<CR>", { desc = "Source current selection" })
 
-set("n", "<M-j>", "<cmd>cnext<CR>")
-set("n", "<M-k>", "<cmd>cprev<CR>")
+set("n", "<Esc>", "<cmd>nohlsearch<CR>")
 
-set("n", "-", "<cmd>lua MiniFiles.open()<CR>")
+set("n", "<leader>q", vim.diagnostic.setloclist, { desc = "Open diagnostic [Q]uickfix list" })
+
+set("n", "<M-j>", "<cmd>cnext<CR>", { desc = "Quickfix list next entry" })
+set("n", "<M-k>", "<cmd>cprev<CR>", { desc = "Quickfix list previous entry" })
+
+set("n", "-", "<cmd>lua MiniFiles.open()<CR>", { desc = "Open Mini.files" })
 
 
-set("t", "<esc><esc>", "<c-\\><c-n>")
+set("t", "<Esc><Esc>", "<C-\\><C-n>", { desc = "Exit terminal mode" })
