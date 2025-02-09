39e92a30fdd48ce340d4bf57d2f50dead1ccad1c
Jochim Van Dijck <jochim.van.dijck@gmail.com>
2025-02-09 18:16:06 +0100

    update Neovim options

--- .config/nvim/plugin/options.lua
+++ .config/nvim/plugin/options.lua
@@ -1,10 +1,17 @@
 local opt = vim.opt
 
+opt.showmode = false
 
 opt.inccommand = "split"
 opt.smartcase = true
 opt.ignorecase = true
 
+opt.updatetime = 250
+
+opt.list = true
+opt.listchars = { tab = '» ', trail = '·', nbsp = '␣' }
+
+opt.scrolloff = 10
 
 opt.number = true
 opt.relativenumber = true
@@ -12,7 +19,6 @@ opt.relativenumber = true
 opt.splitbelow = true
 opt.splitright = true
 
-opt.signcolumn = "yes"
 opt.shada = { "'10", "<0", "s10", "h" }
 
 opt.clipboard = "unnamedplus"
@@ -24,3 +30,7 @@ opt.linebreak = true
 
 opt.tabstop = 4
 opt.shiftwidth = 4
+
+opt.numberwidth = 3
+opt.signcolumn = "yes:1"
+opt.statuscolumn = "%l%s"
