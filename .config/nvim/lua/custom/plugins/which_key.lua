a93848e5b1667294cfee00fa040203f35b69149e
Jochim Van Dijck <jochim.van.dijck@gmail.com>
2025-02-10 00:26:13 +0100

    update which-key plugin config

--- .config/nvim/lua/custom/plugins/which_key.lua
+++ .config/nvim/lua/custom/plugins/which_key.lua
@@ -1,24 +1,22 @@
 return {
   {
     "folke/which-key.nvim",
-    event = "VimEnter",
+    event = "VeryLazy",
     opts = {
+      preset = "helix",
       -- delay between pressing a key and opening which-key (milliseconds)
       delay = 250,
       icons = {
         mappings = vim.g.have_nerd_font,
-        keys = {},
       },
-
-      -- Document existing key chains
-      spec = {
-        { "<leader>c", group = "[C]ode",     mode = { "n", "x" } },
-        { "<leader>d", group = "[D]ocument" },
-        { "<leader>r", group = "[R]ename" },
-        { "<leader>s", group = "[S]earch" },
-        { "<leader>w", group = "[W]orkspace" },
-        { "<leader>t", group = "[T]oggle" },
-        { "<leader>h", group = "Git [H]unk", mode = { "n", "v" } },
+    },
+    keys = {
+      {
+        "<leader>?",
+        function()
+          require("which-key").show({ global = false })
+        end,
+        desc = "Buffer Local Keymaps",
       },
     },
   }
