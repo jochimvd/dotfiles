02d8ef771601ae3a43e37576dc35b10c7e5e5308
Jochim Van Dijck <jochim.van.dijck@gmail.com>
2025-02-09 18:18:08 +0100

    add mini.git and mini.diff plugin configurations

--- .config/nvim/lua/custom/plugins/mini.lua
+++ .config/nvim/lua/custom/plugins/mini.lua
@@ -4,6 +4,12 @@ return {
     version = false,
     config = function()
       require("mini.statusline").setup()
+      require("mini.git").setup()
+      require("mini.diff").setup({
+        view = {
+          signs = { add = "│", delete = "│", change = "│" },
+        }
+      })
       require("mini.icons").setup()
       require("mini.pick").setup()
       require("mini.files").setup({
