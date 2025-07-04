75b1d31a785349947b05f7cabdfc2755930f8aaa
Jochim Van Dijck <jochim.van.dijck@gmail.com>
2025-07-05 01:19:30 +0200

    remove tofi

--- install/10-hypr.sh
+++ install/10-hypr.sh
@@ -3,7 +3,7 @@
 paru -S --noconfirm --needed \
     hyprland hyprpaper hypridle hyprlock hyprpicker hyprshot hyprpolkitagent hyprland-qtutils \
     xdg-desktop-portal-gtk xdg-desktop-portal-hyprland qt5-wayland qt6-wayland \
-    tofi rofi-wayland waybar swaync \
+    rofi-wayland waybar swaync \
     uwsm
 
 systemctl --user enable --now hyprpolkitagent.service
