816faf9322961b21ab087e71345a890096507af7
Jochim Van Dijck <jochim.van.dijck@gmail.com>
2025-07-04 20:42:50 +0200

    got tricked by bash expansion sorting

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
-for f in ~/.local/share/dotfiles/install/*.sh; do source "$f"; done
+for f in $(find ~/.local/share/dotfiles/install/ -maxdepth 1 -name "*.sh" | sort -V); do source "$f"; done
 
 echo "Please reboot to complete installation..."
