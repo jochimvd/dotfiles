3c60e060e21b60e2de03787f03eff4290dd10bca
Jochim Van Dijck <jochim.van.dijck@gmail.com>
2025-07-05 01:55:50 +0200

    suppress directory exists warning

--- install/10-docker.sh
+++ install/10-docker.sh
@@ -2,7 +2,7 @@
 
 paru -S --noconfirm --needed docker docker-compose
 
-sudo mkdir /etc/docker
+sudo mkdir -p /etc/docker
 
 # Limit log size to avoid running out of disk
 echo '{"log-driver":"json-file","log-opts":{"max-size":"10m","max-file":"5"}}' | sudo tee /etc/docker/daemon.json
