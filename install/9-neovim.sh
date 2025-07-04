a750fe9f5e2bb1c4224def8ba54689a4a83b40a3
Jochim Van Dijck <jochim.van.dijck@gmail.com>
2025-07-04 22:53:39 +0200

    refactor neovim installation

--- install/9-neovim.sh
+++ install/9-neovim.sh
@@ -5,21 +5,25 @@ paru -S --noconfirm --needed base-devel cmake ninja curl
 NVIM_SRC_DIR="$HOME/.local/share/src/neovim"
 mkdir -p "$(dirname "$NVIM_SRC_DIR")"
 
+COMPILE=false
+
 if [ -d "$NVIM_SRC_DIR" ]; then
     read -p "Neovim source directory found. Recompile? (y/N) " -n 1 -r
 
-    if [[ ! $REPLY =~ ^[Yy]$ ]]; then
-        echo "Skipping Neovim compilation."
-        exit 0
+    if [[ $REPLY =~ ^[Yy]$ ]]; then
+        COMPILE=true
     fi
 else
     git clone https://github.com/neovim/neovim "$NVIM_SRC_DIR"
+    COMPILE=true
 fi
 
-cd "$NVIM_SRC_DIR"
-git fetch --prune --tags --force
-git checkout stable
-sudo make distclean
-make CMAKE_BUILD_TYPE=Release
-sudo make install
-cd - > /dev/null
+if [ "$COMPILE" = true ]; then
+    cd "$NVIM_SRC_DIR"
+    git fetch --prune --tags --force
+    git checkout stable
+    sudo make distclean
+    make CMAKE_BUILD_TYPE=Release
+    sudo make install
+    cd - > /dev/null
+fi
