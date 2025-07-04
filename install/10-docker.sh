bb0f63d30b35233b48d78f39f10d0aca7d56b791
Jochim Van Dijck <jochim.van.dijck@gmail.com>
2025-07-05 01:40:46 +0200

    add missing directory creation for Docker configuration

--- install/10-docker.sh
+++ install/10-docker.sh
@@ -2,6 +2,8 @@
 
 paru -S --noconfirm --needed docker docker-compose
 
+sudo mkdir /etc/docker
+
 # Limit log size to avoid running out of disk
 echo '{"log-driver":"json-file","log-opts":{"max-size":"10m","max-file":"5"}}' | sudo tee /etc/docker/daemon.json
 
