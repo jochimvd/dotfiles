191c113b5ac7d026e1512fea24ef459f5f94e518
Jochim Van Dijck <jochim.van.dijck@gmail.com>
2025-07-04 22:21:11 +0200

    add systemctl commands to desktop install script

--- install/9-desktop.sh
+++ install/9-desktop.sh
@@ -6,3 +6,10 @@ paru -S --noconfirm --needed \
     nautilus gnome-calculator gnome-keyring \
     seahorse gearlever \
     zen-browser-bin
+
+sudo systemctl enable --now NetworkManager.service
+sudo systemctl enable --now bluetooth.service
+
+# set locale
+sudo localectl set-locale LANG=en_US.UTF-8
+sudo localectl set-locale LC_TIME=en_GB.UTF-8
