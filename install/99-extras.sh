a81e4e59771e73d177df77e8bffe13fc07d8a1b1
Jochim Van Dijck <jochim.van.dijck@gmail.com>
2025-07-05 01:53:09 +0200

    switch to 1password beta for wayland clipboard support

--- install/99-extras.sh
+++ install/99-extras.sh
@@ -3,5 +3,5 @@
 paru -S --noconfirm --needed \
     signal-desktop discord spotify-launcher \
     obsidian libreoffice-still obs-studio vlc \
-    1password 1password-cli \
+    1password-beta 1password-cli \
     satty pinta
