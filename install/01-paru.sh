b971ff9c32620d951915a649c6d5794e800b69d3
Jochim Van Dijck <jochim.van.dijck@gmail.com>
2025-07-15 12:48:03 +0200

    add color to pacman

--- install/01-paru.sh
+++ install/01-paru.sh
@@ -1,10 +1,14 @@
 #!/usr/bin/env bash
 
+sudo pacman -S --needed --noconfirm base-devel
+
 if ! command -v paru &> /dev/null; then
-    sudo pacman -S --needed --noconfirm base-devel
     git clone https://aur.archlinux.org/paru.git /tmp/paru || { echo "Failed to clone paru"; exit 1; }
     cd /tmp/paru
     makepkg -si
     cd - > /dev/null
     rm -rf /tmp/paru
+
+    # add color to pacman
+    sudo sed -i '/^\[options\]/a Color' /etc/pacman.conf
 fi
