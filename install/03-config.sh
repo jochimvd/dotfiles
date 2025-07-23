615dbeede53c438da4a64f2e0a6481351782fefd
Jochim Van Dijck <jochim.van.dijck@brain2.com>
2025-07-23 13:20:31 +0200

    add keyboard battery monitor service

--- install/03-config.sh
+++ install/03-config.sh
@@ -4,6 +4,8 @@
 cp -R ~/.local/share/dotfiles/config/* ~/.config/
 echo "source ~/.config/zsh/.zshenv" >~/.zshenv
 
+systemctl --user enable --now keyboard-battery-monitor.timer
+
 # Copy .desktop files
 mkdir -p ~/.local/share/applications
 cp ~/.local/share/dotfiles/share/applications/*.desktop ~/.local/share/applications/
