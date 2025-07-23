c4e4d06095e55d84e4686a677867945c8b5db367
Jochim Van Dijck <jochim.van.dijck@brain2.com>
2025-07-23 10:21:01 +0200

    hide some apps from launcher

--- install/03-config.sh
+++ install/03-config.sh
@@ -1,4 +1,12 @@
 #!/usr/bin/env bash
 
+# Copy config
 cp -R ~/.local/share/dotfiles/config/* ~/.config/
 echo "source ~/.config/zsh/.zshenv" >~/.zshenv
+
+# Copy .desktop files
+mkdir -p ~/.local/share/applications
+cp ~/.local/share/dotfiles/share/applications/*.desktop ~/.local/share/applications/
+cp ~/.local/share/dotfiles/share/applications/hidden/*.desktop ~/.local/share/applications/
+
+update-desktop-database ~/.local/share/applications
