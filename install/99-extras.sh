fda3742950fb4b1fdf27a5ef14ddf122157c872b
Jochim Van Dijck <jochim.van.dijck@brain2.com>
2025-07-23 10:52:59 +0200

    switch to obsidian-bin

--- install/99-extras.sh
+++ install/99-extras.sh
@@ -2,6 +2,6 @@
 
 paru -S --noconfirm --needed \
     signal-desktop discord spotify-launcher \
-    obsidian libreoffice-still obs-studio vlc \
+    obsidian-bin libreoffice-still obs-studio vlc \
     1password-beta 1password-cli \
     satty pinta
