9dd775e6eb3bc46c4b66d8f675d196a89b99fd86
Jochim Van Dijck <jochim.van.dijck@gmail.com>
2025-09-12 14:13:10 +0200

    switch to walker

--- install/10-hypr.sh
+++ install/10-hypr.sh
@@ -3,7 +3,7 @@
 paru -S --noconfirm --needed \
     hyprland hyprpaper hypridle hyprlock hyprpicker hyprshot hyprpolkitagent hyprland-qtutils \
     xdg-desktop-portal-gtk xdg-desktop-portal-hyprland qt5-wayland qt6-wayland \
-    walker-bin libqalculate \
+    walker libqalculate \
     waybar swaync \
     uwsm
 
