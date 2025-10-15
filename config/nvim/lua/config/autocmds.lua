852bd1485cba295b780afbdf966aac4c8a5d6c48
Jochim Van Dijck <jochim.van.dijck@gmail.com>
2025-10-16 00:52:57 +0200

    switch between relative and absolute line numbers based on mode

--- config/nvim/lua/config/autocmds.lua
+++ config/nvim/lua/config/autocmds.lua
@@ -6,7 +6,7 @@ vim.api.nvim_create_autocmd("TextYankPost", {
   end,
 })
 
--- wrap and check for spell in text filetypes
+-- Wrap and check for spell in text filetypes
 vim.api.nvim_create_autocmd("FileType", {
   group = vim.api.nvim_create_augroup('wrap_spell', { clear = true }),
   pattern = { "text", "plaintex", "typst", "gitcommit", "markdown" },
@@ -15,3 +15,38 @@ vim.api.nvim_create_autocmd("FileType", {
     vim.opt_local.spell = true
   end,
 })
+
+
+
+-- Enable relative line numbers only in normal mode
+local numbertoggle = vim.api.nvim_create_augroup("number_toggle", { clear = true })
+
+-- enable relative numbers when entering a buffer/window or leaving insert mode
+vim.api.nvim_create_autocmd(
+  { "BufEnter", "FocusGained", "InsertLeave", "WinEnter" },
+  {
+    group = numbertoggle,
+    pattern = "*",
+    callback = function()
+      if vim.wo.number and vim.fn.mode() ~= "i" then
+        -- buffnr
+        vim.wo.relativenumber = true
+      end
+    end,
+  }
+)
+
+-- disable relative numbers when leaving a buffer/window or entering insert mode
+vim.api.nvim_create_autocmd(
+  { "BufLeave", "FocusLost", "InsertEnter", "WinLeave" },
+  {
+    group = numbertoggle,
+    pattern = "*",
+    callback = function()
+      if vim.wo.number then
+        vim.wo.relativenumber = false
+      end
+    end,
+  }
+)
+
