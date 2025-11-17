2df199e61849bafe8f1c42088317b44dccb6da28
Jochim Van Dijck <jochim.van.dijck@gmail.com>
2025-11-17 22:40:21 +0100

    hide window buttons in GTK apps

--- install/10-theme.sh
+++ install/10-theme.sh
@@ -4,3 +4,4 @@ paru -S --noconfirm --needed kvantum-qt5 gnome-themes-extra
 
 gsettings set org.gnome.desktop.interface gtk-theme "Adwaita-dark"
 gsettings set org.gnome.desktop.interface color-scheme "prefer-dark"
+gsettings set org.gnome.desktop.wm.preferences button-layout ":"
