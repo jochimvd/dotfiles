d3ca531bec311f3eb902b52071743fa2eded7070
Jochim Van Dijck <jochim.van.dijck@gmail.com>
2025-11-26 12:33:29 +0100

    switch to iwd

--- install/10-desktop.sh
+++ install/10-desktop.sh
@@ -2,13 +2,13 @@
 
 paru -S --noconfirm --needed \
     brightnessctl playerctl wireplumber power-profiles-daemon \
-    pavucontrol network-manager-applet bluetui udiskie \
+    pavucontrol iwd impala bluetui udiskie \
     nautilus papers gnome-calculator gnome-keyring seahorse \
     grim slurp wayfreeze-git satty wl-screenrec \
     gearlever \
     chromium zen-browser-bin
 
-sudo systemctl enable --now NetworkManager.service
+sudo systemctl enable --now iwd.service
 sudo systemctl enable --now bluetooth.service
 sudo systemctl enable --now power-profiles-daemon
 
