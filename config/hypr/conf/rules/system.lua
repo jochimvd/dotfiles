db64bb64f1c0148d96103f82e1fa102cebe06683
Jochim Van Dijck <jochim.van.dijck@gmail.com>
2026-05-29 21:04:09 +0200

    update floating window regex

--- config/hypr/conf/rules/system.lua
+++ config/hypr/conf/rules/system.lua
@@ -8,7 +8,7 @@ hl.window_rule({ match = { class = "(com.gabm.satty|imv|mpv)" }, tag = "+floatin
 hl.window_rule({
     match = {
         class = "(xdg-desktop-portal-gtk|org.gnome.Nautilus)",
-        title = "^(Open.*Files?|Open [F|f]older.*|Save.*Files?|Save.*As|Save|All Files|.*wants to [open|save].*|[C|c]hoose.*|File Upload.*)",
+        title = "^(Open.*Files?|Open [F|f]older.*|Save.*Files?|Save.*As.*|Save|All Files|.*wants to [open|save].*|[C|c]hoose.*|File Upload.*)",
     },
     tag = "+floating-window",
 })
