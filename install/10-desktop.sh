34d64019c8d172141a2592c8f823002441d86152
Jochim Van Dijck <jochim.van.dijck@gmail.com>
2026-08-11 02:03:41 +0200

    add tailscale and syncthing

--- install/10-desktop.sh
+++ install/10-desktop.sh
@@ -6,11 +6,21 @@ paru -S --noconfirm --needed \
     nautilus sushi papers gnome-calculator gnome-keyring seahorse \
     grim slurp wayfreeze-git satty wl-screenrec \
     gearlever \
-    chromium zen-browser-bin
+    chromium zen-browser-bin \
+    tailscale syncthing
 
 sudo systemctl enable --now iwd.service
 sudo systemctl enable --now bluetooth.service
 sudo systemctl enable --now power-profiles-daemon
+sudo systemctl enable --now tailscaled.service
+systemctl --user enable --now syncthing.service
+
+sudo tailscale set --operator="$USER"
+tailscale configure systray --enable-startup=systemd
+systemctl --user daemon-reload
+systemctl --user enable tailscale-systray.service
+systemctl --user restart tailscale-systray.service
+
 
 sudo localectl set-locale \
     LANG=en_US.UTF-8 \
